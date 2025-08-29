9b67d1c2c413713e5889f8128729839f4e02e30e
ekowmensah <ekowme@gmail.com>
2025-08-29 13:01:57 +0000

    git push

--- helpers/hubtel_status.php
+++ helpers/hubtel_status.php
@@ -78,10 +78,10 @@ function check_hubtel_transaction_status($transaction_id, $client_reference = nu
     }
 
     // Use the correct Hubtel transaction status endpoint
-    // Primary endpoint uses clientReference as mandatory parameter
-    $url = "https://api-txnstatus.hubtel.com/transactions/{$transaction_id}/status";
+    // The API expects client_reference in the URL path, not the hubtel_transaction_id
+    $url = "https://api-txnstatus.hubtel.com/transactions/{$client_reference}/status";
     
-    // Add clientReference as mandatory parameter (preferred by Hubtel)
+    // Add clientReference as query parameter as well (as per documentation)
     if ($client_reference) {
         $url .= "?clientReference=" . urlencode($client_reference);
     }
