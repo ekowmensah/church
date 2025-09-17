cc02ea9b648797258c9365565362492b32d49639
ekowmensah <ekowme@gmail.com>
2025-09-17 00:04:33 +0000

    git push

--- views/user_form.php
+++ views/user_form.php
@@ -510,8 +510,7 @@ ob_start();
     </div>
   </div>
 </div>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+<!-- jQuery and Bootstrap already loaded in layout.php -->
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 <script>
